5b8d50c4f49a005ca054679877fbd9e96e1fbf03
Sjur Hernes <sjhernes@gmail.com>
2010-06-11 01:59:26 +0200

    finpuss på generiske typer

--- inf1010/code/generics.java
+++ inf1010/code/generics.java
@@ -1,20 +1,19 @@
 class List<E>{
     Node root;
+    
     class Node{
-	Node(E ee){e=ee}
-	E e;
+	Node(E e) lagretObjekt = e; 
+	E lagretObjekt;
 	Node next;
-    }
-
+    }    
     void put(E e){
-	Node n=new Node(e)
-	if(root==null) root=n;
+	Node n = new Node(e);
+	if(root == null) root = n;
 	else{ 
 	    n.next = root; 
 	    root = n;
 	}
-    }
-
+    }    
     E pop(){
 	E tmp = root.e;
 	root = root.next;
