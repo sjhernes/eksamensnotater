75f8ae15c11e2d2c99ff03a171678fa26b8ea0dd
martiben <bulltacho@hotmail.com>
2010-06-14 00:37:41 +0200

    ..

---
+++ inf1010/code/generics3.java
@@ -0,0 +1,5 @@
+class List<E extends Comparable<E>>{
+
+/** Hvis vi nå extender nodeklassen vår med Comparable<E> som vist over, kan vi bruke 
+  * compareTo(); som vi definerte i 
+  * Person klassen, til å sammenlikne E objekter av typen Person. */
